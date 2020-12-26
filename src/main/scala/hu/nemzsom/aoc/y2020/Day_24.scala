e73bc112ae00e8afbde4c7a6ea896f649429d563
Zsombor <zsombor.nemestothy@falcon.io>
2020-12-26 08:57:27 +0100

    Day 24 part 2

--- src/main/scala/hu/nemzsom/aoc/y2020/Day_24.scala
+++ src/main/scala/hu/nemzsom/aoc/y2020/Day_24.scala
@@ -10,12 +10,21 @@ object Day_24 extends App with Solver {
   type Steps = List[Direction]
 
   override def solve(input: List[String]) = {
-    val plane = Plane()
-    Input.parse(input).flips.foreach(plane.flip)
-    plane.countBlackTiles.toString
+    val pane = flipPane(Input.parse(input))
+    pane.countBlackTiles.toString
   }
 
-  override def solveSecondPart(input: List[String]) = ???
+  override def solveSecondPart(input: List[String]) = {
+    val pane = flipPane(Input.parse(input))
+    Range(0, 100).foreach(_ => pane.nextPattern())
+    pane.countBlackTiles.toString
+  }
+
+  def flipPane(in: Input): Pane = {
+    val plane = Pane()
+    in.flips.foreach(plane.flip)
+    plane
+  }
 
   sealed trait Direction {
     def pos: Pos
@@ -38,13 +47,14 @@ object Day_24 extends App with Solver {
   case object NorthEast extends Direction { val pos: Pos = Pos(1, -1, 0)}
   case class Pos(x: Int, y: Int, z: Int) {
     def +(pos: Pos): Pos = Pos(x + pos.x, y + pos.y, z + pos.z)
+    def neighbours: Set[Pos] = Set(East, SouthEast, SouthWest, West, NorthWest, NorthEast).map(this + _.pos)
   }
 
   object Pos {
     val center: Pos = Pos(0, 0, 0)
   }
 
-  case class Plane(blackTiles: mutable.Set[Pos] = mutable.Set()) {
+  case class Pane(blackTiles: mutable.Set[Pos] = mutable.Set()) {
 
     def flip(steps: Steps): Unit = {
       val endPos = steps.foldLeft(Pos.center){ case (pos, direction) =>
@@ -54,6 +64,26 @@ object Day_24 extends App with Solver {
       else blackTiles.add(endPos)
     }
 
+    def nextPattern(): Unit = {
+      val nextBlacks = blackTiles.flatMap(pos => pos.neighbours + pos).flatMap(pos =>
+        if (isBlack(pos)) calculateBlack(pos)
+        else calculateWhite(pos)
+      )
+      blackTiles.clear()
+      blackTiles.addAll(nextBlacks)
+    }
+
+    def calculateBlack(black: Pos): Option[Pos] = {
+      val bn = blackNeighbours(black)
+      if (bn == 0 || bn > 2) None
+      else Some(black)
+    }
+    def calculateWhite(white: Pos): Option[Pos] =
+      if (blackNeighbours(white) == 2) Some(white)
+      else None
+
+    def isBlack(pos: Pos): Boolean = blackTiles.contains(pos)
+    def blackNeighbours(pos: Pos): Int = pos.neighbours.count(isBlack)
     def countBlackTiles: Int = blackTiles.size
   }
 
